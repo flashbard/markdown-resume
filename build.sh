a846a2dbca4cf9c19c5fdada507827194a884711
flashbard <flashbard@gmail.com>
2023-02-06 21:06:38 +0530

    some additional changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pandoc -o resume.html -c resume-css-stylesheet.css --standalone resume.md 
+pandoc -o resume.html -c resume-css-stylesheet.css --standalone resume.md  --metadata=title:"Sampath Shanmugam"
 wkhtmltopdf --enable-local-file-access resume.html resume.pdf
